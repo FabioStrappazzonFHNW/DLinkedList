0f558dd3a47c63088715420affb5a48425790ee9
Fabio <fabiostra@hotmail.com>
2016-10-17 23:04:00 +0200

    removed linear complexity

--- src/algd2/exercise3/DLinkedList.java
+++ src/algd2/exercise3/DLinkedList.java
@@ -20,53 +20,51 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 
 	@Override
 	public boolean checkMembership(ListItem item) {
-		// TODO Auto-generated method stub
+		for(Element<E> current = head; current != tail; current= current.next)
+			if(item.get() == current)
+				return true;
 		return false;
 	}
 
 	@Override
-	public ListItem head() {
+	public ListItem<E> head() {
 		return new ListItem<E>(head);
 	}
 
 	@Override
-	public ListItem tail() {
+	public ListItem<E> tail() {
 		return new ListItem<E>(tail);
 	}
 
 	@Override
-	public ListItem next(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
+	public ListItem<E> next(ListItem item) {
 		return item.getNext();
 	}
 
 	@Override
-	public ListItem previous(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
+	public ListItem<E> previous(ListItem item) {
 		return item.getPrevious();
 	}
 
 	@Override
-	public ListItem cyclicNext(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
+	public ListItem<E> cyclicNext(ListItem item) {
 		return item.getNext() != null ? item.getNext() : head();
 	}
 
 	@Override
-	public ListItem cyclicPrevious(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
+	public ListItem<E> cyclicPrevious(ListItem item) {
 		return item.getPrevious() != null ? item.getPrevious() : tail();
 	}
 
 	@Override
-	public ListItem delete(ListItem item, boolean next) {
+	public ListItem<E> delete(ListItem item, boolean next) {
 		if (! checkMembership(item)) throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ListItem cyclicDelete(ListItem item, boolean next) {
+	public ListItem<E> cyclicDelete(ListItem item, boolean next) {
 		if (! checkMembership(item)) throw new IllegalArgumentException();
 		// TODO Auto-generated method stub
 		return null;
@@ -74,24 +72,17 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 
 	@Override
 	public E get(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
 		return (E) item.get();
 	}
 
 	@Override
 	public void set(ListItem item, E data) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
 		item.set(data);
 	}
 
 	@Override
 	public E remove(ListItem item) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
-		if (item.get() == tail) {
-			tail = (Element<E>) item.getNext().get();
-		} else if (item.get() == head) {
-			head = (Element<E>) item.getPrevious().get();
-		}
+		
 		size--;
 		return (E) item.delete();
 	}
@@ -112,7 +103,7 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 	}
 
 	@Override
-	public ListItem addTail(E data) {
+	public ListItem<E> addTail(E data) {
 		Element<E> newE = new Element<>(data);
 		newE.prev = tail;
 		tail = newE;
@@ -121,32 +112,35 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 	}
 
 	@Override
-	public ListItem addAfter(ListItem item, E data) {
-		if (! checkMembership(item)) throw new IllegalArgumentException();
+	public ListItem<E> addAfter(ListItem item, E data) {
 		if (item.getNext() == null) {
-			return addHead(data);
+			return addTail(data);
 		} else {
 			Element<E> newE = new Element<>(data);
 			item.linkInAfter(newE);
+			size++;
 			return new ListItem(newE);
 		}
 	}
 
 	@Override
-	public ListItem addBefore(ListItem item, E data) {
+	public ListItem<E> addBefore(ListItem item, E data) {
 		if (! checkMembership(item)) throw new IllegalArgumentException();
 		if (item.getPrevious() == null) {
 			return addTail(data);
 		} else {
 			Element<E> newE = new Element<>(data);
 			item.linkInBefore(newE);
+			size++;
 			return new ListItem(newE);
 		}
 	}
 
 	@Override
 	public void moveToHead(ListItem item) {
-		// TODO Auto-generated method stub
+		Element<E> e = (Element) item.get();
+		this.delete(item, true);
+		this.addHead(e.e);
 		
 	}
 
